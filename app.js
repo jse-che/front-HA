cac04c25afdfec2188ebc9e826efd05cc7333836
Jose Charris <jose.charris@horus-sc.com>
2026-01-28 10:54:39 -0500

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const WS_URL = "wss://AQUI_TU_NGROK_URL"; // 👈 MISMA del server
-const TOKEN = "MI_TOKEN_SIMPLE"; // el que validas en server.js
+const WS_URL = "wss://c558e2d08663.ngrok-free.app"; // 👈 MISMA del server
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlYzY0OTg1ZWEzOWE0ZmFkOGI4ZDJmYmIyMjFhNmM4OCIsImlhdCI6MTc2OTUzMTEwOSwiZXhwIjoyMDg0ODkxMTA5fQ.fBFH_4POEB5QWl9rcN1MjaGB8tBgiBNreVMHADtLBVo"; // el que validas en server.js
 
-const log = (msg) => {
-  document.getElementById("log").textContent += msg + "\n";
-};
+const ENTITY_ID = "switch.it_hab_2_2";
+
+const toggle = document.getElementById("toggle");
+const status = document.getElementById("status");
+const log = document.getElementById("log");
+
+let internalChange = false;
 
 const ws = new WebSocket(WS_URL);
 
 ws.onopen = () => {
-  log("🟢 Conectado al servidor");
-
+  status.textContent = "Conectado";
   ws.send(JSON.stringify({
     type: "auth",
     token: TOKEN
@@ -18,23 +21,26 @@ ws.onopen = () => {
 
 ws.onmessage = (e) => {
   const data = JSON.parse(e.data);
-  log("📩 " + JSON.stringify(data, null, 2));
+  log.textContent += JSON.stringify(data) + "\n";
+
+  // Evento desde Home Assistant
+  if (
+    data.type === "ha_event" &&
+    data.entity_id === ENTITY_ID
+  ) {
+    internalChange = true;
+    toggle.checked = data.new_state === "on";
+    internalChange = false;
+  }
 };
 
-function turnOn() {
-  ws.send(JSON.stringify({
-    type: "call_service",
-    domain: "switch",
-    service: "turn_on",
-    entity_id: "switch.it_hab_2_2"
-  }));
-}
+toggle.addEventListener("change", () => {
+  if (internalChange) return;
 
-function turnOff() {
   ws.send(JSON.stringify({
     type: "call_service",
     domain: "switch",
-    service: "turn_off",
-    entity_id: "switch.it_hab_2_2"
+    service: toggle.checked ? "turn_on" : "turn_off",
+    entity_id: ENTITY_ID
   }));
-}
+});
